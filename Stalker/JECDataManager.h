a629060c574ad6d3f3b6aceca5d243eaab95d3bf
JoshCooper417 <joshcooper417@gmail.com>
2013-02-18 10:11:33 -0500

    Added lots of functionality

---
+++ Stalker/JECDataManager.h
@@ -0,0 +1,13 @@
+//
+//  JECDataManager.h
+//  Stalker
+//
+//  Created by Joshua Cooper on 2/17/13.
+//  Copyright (c) 2013 Joshua Cooper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JECDataManager : NSObject
+
+@end
---
+++ Stalker/JECDataManager.m
@@ -0,0 +1,13 @@
+//
+//  JECDataManager.m
+//  Stalker
+//
+//  Created by Joshua Cooper on 2/17/13.
+//  Copyright (c) 2013 Joshua Cooper. All rights reserved.
+//
+
+#import "JECDataManager.h"
+
+@implementation JECDataManager
+
+@end
